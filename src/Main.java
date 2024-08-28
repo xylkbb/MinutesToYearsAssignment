aa26bb85675bf818a222ff43c0a95638d4aa8d2c
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-28 08:07:49 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+
+    }
+
+    private static void printYearsAndDays(long minutes) {
+        long hours = minutes / 60;
+        long days = hours / 24;
+        long years = days / 365;
+        long leftoverDays = days % 365;
+
+
+        System.out.println(minutes + " min = " + years + " years and " + leftoverDays + " days");
+    }
+
+
 }
